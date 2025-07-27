3ed4af36bd11a3cce35635c1ffde81238f6f1044
mridha-rakib <rakibmahmudmridha@gmail.com>
2025-07-27 10:19:43 +0600

    [server]: add message database document

---
+++ server/src/service/message.service.js
@@ -0,0 +1,6 @@
+import Message from "#app/models/message.model";
+import Conversation from "#app/models/conversation.model";
+import { NotFoundException, BadRequestException } from "#app/utils/error-handler.utils";
+import { ErrorCodeEnum } from "#app/enums/error-code.enum";
+
+class MessageService {}
