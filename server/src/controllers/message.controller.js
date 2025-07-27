3f0f0c579d12f964e6c9784615096427b2227fca
mridha-rakib <rakibmahmudmridha@gmail.com>
2025-07-27 11:41:59 +0600

    [server]: add socket functionality

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -9,7 +9,21 @@ const sendMessage = asyncHandler(async (req, res) => {
   const senderId = req.user.userId;
 
   const conversation = await MessageService.findOrCreateConversation(senderId, receiverId);
-  
+
+  const newMessage = await MessageService.createMessage({
+    senderId,
+    receiverId,
+    message,
+  });
+
+  const savedMessage = await MessageService.saveMessageAndConversation(conversation, newMessage);
+  const messageResponse = MessageService.formatMessageResponse(savedMessage);
+
+  res.status(HTTPSTATUS.CREATED).json({
+    success: true,
+    message: "Message sent successfully",
+    data: messageResponse,
+  });
 });
 
 export const MessageController = { sendMessage };
---
+++ server/src/socket/socket.js
@@ -0,0 +1,33 @@
+import { Server, Socket } from "socket.io";
+import http from "http";
+import app from "#app/app";
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: [env.CORS_ORIGIN || "*"],
+    credentials: true,
+  },
+});
+
+export const getReceiverSocketId = (receiverId) => {
+  return userSocketMap[receiverId];
+};
+
+const userSocketMap = {};
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if (userId != "undefined") userSocketMap[userId] = socket.id;
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
